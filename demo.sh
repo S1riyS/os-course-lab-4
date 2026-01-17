5533671991426919dd963f3a0408de5aa0bc8643
S1riyS <kirill.ankudinov.94@mail.ru>
2026-01-17 15:03:42 +0300

    Add testcase for large block size

--- demo.sh
+++ demo.sh
@@ -8,6 +8,9 @@ TMP_FILE2="/tmp/test_data2.bin"
 BLOCK_SIZES=(512 1024 4096)
 BLOCK_COUNTS=(1 10 100)
 
+LARGE_BLOCK_SIZE="256k"
+LARGE_BLOCK_COUNT=20
+
 echo "=== VTFS TESTS ==="
 
 # 1. dd из /dev/random куда-нибудь в tmp файл за пределы вашей фс
@@ -36,5 +39,20 @@ for bs in "${BLOCK_SIZES[@]}"; do
     done
 done
 
+echo "Тест: bs=$LARGE_BLOCK_SIZE count=$LARGE_BLOCK_COUNT"
+SOURCE_FILE_LARGE="/tmp/test_data_source_large.bin"
+TMP_FILE2_LARGE="/tmp/test_data2_large.bin"
+dd if=/dev/random of="$SOURCE_FILE_LARGE" bs=$LARGE_BLOCK_SIZE count=$LARGE_BLOCK_COUNT status=progress
+dd if="$SOURCE_FILE_LARGE" of="$MOUNT_POINT/test_${LARGE_BLOCK_SIZE}_${LARGE_BLOCK_COUNT}.bin" bs=$LARGE_BLOCK_SIZE count=$LARGE_BLOCK_COUNT status=none
+dd if="$MOUNT_POINT/test_${LARGE_BLOCK_SIZE}_${LARGE_BLOCK_COUNT}.bin" of="$TMP_FILE2_LARGE" bs=$LARGE_BLOCK_SIZE count=$LARGE_BLOCK_COUNT status=none
+if diff -q "$SOURCE_FILE_LARGE" "$TMP_FILE2_LARGE" > /dev/null; then
+    echo "[OK] Файлы идентичны"
+else
+    echo "[ERROR] Файлы различаются!"
+    rm -f "$SOURCE_FILE_LARGE" "$TMP_FILE2_LARGE"
+    exit 1
+fi
+
+rm -f "$SOURCE_FILE_LARGE" "$TMP_FILE2_LARGE"
 rm -f "$SOURCE_FILE" "$TMP_FILE2"
 echo "=== VTFS TESTS FINISH ==="
